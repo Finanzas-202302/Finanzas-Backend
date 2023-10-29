9d9bfb7bb2a76ce7d07ab0b98df298bc1cc52fec
Sofia Alfaro <alfaros.sofia@gmail.com>
2023-10-29 13:52:44 -0500

    Update: Fix Debt Controller

---
+++ src/main/java/com/upc/Finanzas/Algoritmo.java
@@ -0,0 +1,9 @@
+package com.upc.Finanzas;
+
+import com.upc.Finanzas.model.CalculateDebt;
+
+public class Algoritmo {
+    public void calcularCuotas(CalculateDebt debt_info){
+
+    }
+}
--- src/main/java/com/upc/Finanzas/controller/CalculateDebtController.java
+++ src/main/java/com/upc/Finanzas/controller/CalculateDebtController.java
@@ -3,7 +3,6 @@ package com.upc.Finanzas.controller;
 import com.upc.Finanzas.dto.DebtResultDto;
 import com.upc.Finanzas.dto.EnterDataDebtDto;
 import com.upc.Finanzas.exception.ResourceNotFoundException;
-import com.upc.Finanzas.exception.ValidationException;
 import com.upc.Finanzas.model.CalculateDebt;
 import com.upc.Finanzas.model.Client;
 import com.upc.Finanzas.repository.CalculateDebtRepository;
@@ -13,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import com.upc.Finanzas.Algoritmo;
+import com.upc.Finanzas.controller.ClientController;
 
 import java.util.List;
 
@@ -20,8 +21,11 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/bank/v1/calculate-debt")
 public class CalculateDebtController {
+    Algoritmo algoritmo;
     @Autowired
     private CalculateDebtService calculateDebtService;
+    @Autowired
+    ClientController clientController;
     private final CalculateDebtRepository calculateDebtRepository;
 
     public CalculateDebtController(CalculateDebtRepository calculateDebtRepository) {
@@ -64,10 +68,13 @@ public class CalculateDebtController {
     @Transactional
     @PostMapping
     public ResponseEntity<EnterDataDebtDto> createDataDebt(@RequestBody EnterDataDebtDto enterDataDebtDto){
-        CalculateDebt debt = convertToEntity(enterDataDebtDto);
+        ResponseEntity<List<Client>> responseEntity = clientController.getAllClients();
+        List<Client> clientList = responseEntity.getBody();
+        CalculateDebt debt = convertToEntity(enterDataDebtDto, clientList);
         //validateDebt(debt)
         CalculateDebt createdDebt = calculateDebtService.create(debt);
         EnterDataDebtDto createdEnterDataDebtDto = convertEnterDataToDto(createdDebt);
+        //algoritmo.calcularCuotas(createdDebt);
         return new ResponseEntity<>(createdEnterDataDebtDto, HttpStatus.OK);
     }
 
@@ -90,8 +97,22 @@ public class CalculateDebtController {
                 .build();
     }
 
-    private CalculateDebt convertToEntity(EnterDataDebtDto enterDataDebtDto){
+    private CalculateDebt convertToEntity(EnterDataDebtDto enterDataDebtDto, List<Client> clients){
         CalculateDebt debt = new CalculateDebt();
+        debt.setCoin(enterDataDebtDto.getCoin());
+        debt.setInterest_rate(enterDataDebtDto.getInterest_rate());
+        debt.setInterest_rate_percentage(enterDataDebtDto.getInterest_rate_percentage());
+        debt.setGrace_period(enterDataDebtDto.getGrace_period());
+        debt.setType_grace_period(enterDataDebtDto.getType_grace_period());
+        debt.setCost_vehicle(enterDataDebtDto.getCost_vehicle());
+        debt.setTerm_of_loan(enterDataDebtDto.getTerm_of_loan());
+        debt.setType_of_term(enterDataDebtDto.getType_of_term());
+        Long clientId = enterDataDebtDto.getClientId();
+        Client client = clients.stream()
+                .filter(c -> c.getId().equals(clientId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + clientId));
+        debt.setClient(client);
         return debt;
     }
 
