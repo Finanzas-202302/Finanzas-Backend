4307c7ac6168b7c8c12f07a1a48669bb87c1b445
Sofia Alfaro <134465624+asofialf@users.noreply.github.com>
2023-11-18 00:25:06 -0500

    Update CorsConfig.java

--- src/main/java/com/upc/Finanzas/config/CorsConfig.java
+++ src/main/java/com/upc/Finanzas/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig{
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                        .allowedOrigins("http://localhost:4200")
+                registry.addMapping("/api/bank/v1/**")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
 
